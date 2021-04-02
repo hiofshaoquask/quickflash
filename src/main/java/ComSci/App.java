1d07c3dc399de973a356e6180b43083db423679f
hiofshaoquask <45333647+hiofshaoquask@users.noreply.github.com>
2021-04-02 11:09:45 +0700

    Add code to switch root

--- src/main/java/ComSci/App.java
+++ src/main/java/ComSci/App.java
@@ -7,18 +7,26 @@ import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 
 public class App extends Application {
+	private static Scene scene;
+	private static Parent appRoot;
 
     @Override 
     public void start(Stage stage) throws Exception
     {
-        stage.setResizable(false);
         Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+		appRoot = FXMLLoader.load(getClass().getResource("App.fxml"));
         Scene scene = new Scene(root);
 
-        scene.setRoot(root);
         stage.setScene(scene);
         stage.show();
+		
+		stage.setRoot(root);
     }
+
+	public static void login() throws Exception
+	{
+		scene.setRoot(appRoot);
+	}
     
     public static void main(String[] args)
     {
