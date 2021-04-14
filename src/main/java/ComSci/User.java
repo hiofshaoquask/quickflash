0b399b84e4275d52a59f36c411b02ae23e8fa8b4
hiofshaoquask <45333647+hiofshaoquask@users.noreply.github.com>
2021-04-14 15:51:31 +0700

    Added some members to User

--- src/main/java/ComSci/User.java
+++ src/main/java/ComSci/User.java
@@ -1,5 +1,14 @@
 package ComSci;
 
+import java.util.ArrayList;
+
 public abstract class User {
-    protected User() {}
+    static private int userCount = 0;
+    static private ArrayList<Set> totalSet = new ArrayList<Set>();
+    protected String m_name;
+
+    public User()
+    {
+        userCount++;
+    }
 }
