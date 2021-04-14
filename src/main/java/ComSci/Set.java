a6bbe96674194e9504a8b6785d2d8befb9e87d0e
hiofshaoquask <45333647+hiofshaoquask@users.noreply.github.com>
2021-04-14 15:36:37 +0700

    Added add and remove flashcard methods

--- src/main/java/ComSci/Set.java
+++ src/main/java/ComSci/Set.java
@@ -7,16 +7,26 @@ public class Set implements Serializable{
     static final long serialVersionUID = 1L;
 
     private HashMap<String, String> m_flashCards = new HashMap<String, String>();
-    private String m_name;
-    private String m_subject; 
-    private String m_category;
-    private String m_creator;
+    public String m_name;
+    public String m_subject; 
+    public String m_category;
+    public final String m_creator;
     
     public Set(String name, String subject, String creator, String category) 
     {
-        this.m_name = name;
-        this.m_subject = subject;
-        this.m_creator = creator;
-        this.m_category = category;
+        m_name = name;
+        m_subject = subject;
+        m_creator = creator;
+        m_category = category;
+    }
+
+    public void addFlash(String key, String value)
+    {
+        m_flashCards.put(key, value);
+    }
+
+    public void rmvFlash(String key)
+    {
+        m_flashCards.remove(key);
     }
 }
